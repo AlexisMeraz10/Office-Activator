b8bf4f086c26a3049b1e06173381976159b81d87
AlexisMeraz10 <65032318+AlexisMeraz10@users.noreply.github.com>
2025-06-30 09:51:07 -0600

    activator to office that is previously installed but when entered an office app says you dont have the license

---
+++ activator.bat
@@ -0,0 +1,34 @@
+:: Changes to the Office directory according to the installed version
+cd /d "%ProgramFiles%\Microsoft Office\Office16"
+if not exist ospp.vbs (
+    cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16"
+)
+if not exist ospp.vbs (
+    echo Didnt found ospp.vbs in the common paths
+    pause
+    exit /b
+)
+
+:: Obtains the last 5 characters of the installed product key
+for /f "tokens=*" %%A in ('cscript //nologo ospp.vbs /dstatus ^| findstr /i "Last 5 characters"') do (
+    set "line=%%A"
+    set "key=!line:~-5!"
+)
+
+:: Checks if obtained the value
+if "%key%"=="" (
+    echo Cannot obtain the installed key
+    pause
+    exit /b
+)
+
+:: uninstall the current key 
+echo uninstalling key: %key%
+cscript //nologo ospp.vbs /unpkey:%key%
+
+:: Activates Office
+echo Activating Office...
+cscript //nologo ospp.vbs /act
+
+echo process completed.
+pause
